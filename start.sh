252445c8fd0cbc609617885ebcee9c019489ca39
Dikola <44091792+Dpsley@users.noreply.github.com>
2022-08-20 16:08:50 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,30 @@
+clean
+convert gpt
+rem == 1. System partition =========================
+create partition efi size=100
+rem    ** NOTE: For Advanced Format 4Kn drives,
+rem               change this value to size = 260 ** 
+format quick fs=fat32 label="System"
+assign letter="S"
+rem == 2. Microsoft Reserved (MSR) partition =======
+create partition msr size=16
+rem == 3. Windows partition ========================
+rem ==    a. Create the Windows partition ==========
+create partition primary 
+rem ==    b. Create space for the recovery tools ===
+rem       ** Update this size to match the size of
+rem          the recovery tools (winre.wim)
+rem          plus some free space.
+shrink minimum=500
+rem ==    c. Prepare the Windows partition ========= 
+format quick fs=ntfs label="Windows"
+assign letter="W"
+rem === 4. Recovery partition ======================
+create partition primary
+format quick fs=ntfs label="Recovery"
+assign letter="R"
+set id="de94bba4-06d1-4d40-a16a-bfd50179d6ac"
+gpt attributes=0x8000000000000001
+lis vol
+lis par
+exit
